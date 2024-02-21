d1dae0f1cfcbb306554c335c9c703c8e54102fa1
Adarsh Sinha <75273319+AdarshSinha7@users.noreply.github.com>
2024-02-21 20:00:23 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("hello") //we are using node JS not browser
+
+console.log(3
+    +3)//code readbility should be high
+
+console.log("Adarsh")
+
+
+let name="Adarsh"
+let age=21
+let isLoggesIn= false
+
+//number => 2 to the power 53
+//bigInt
+//string =>""
+//boolean =>true/fasle
+//null =>it is a standalone value
+//unefinedd =>
+//symbol =>unique
+
+//object
+
+console.log(typeof "Adarsh");
+
+
+console.log(typeof null)//null is an object
+
