eb4987d84f247109c277613e8c1d4080143d46c1
Adarsh Sinha <75273319+AdarshSinha7@users.noreply.github.com>
2024-04-19 17:13:50 +0000

    necessary msg

--- 01_basics/comparision.js
+++ 01_basics/comparision.js
@@ -1,15 +1,15 @@
-// console.log(2>1);
-// console.log(2>=1);
+// // console.log(2>1);
+// // console.log(2>=1);
 
-console.log("2">1);
+// console.log("2">1);
 
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
 
-//types of memory
+// //types of memory
 
-//Stack(Primitive),  Heap(Non-Primitive)
+// //Stack(Primitive),  Heap(Non-Primitive)
 
 
