11653c542f17401ebd70da6b9162d9b69840dca3
Adarsh Sinha <75273319+AdarshSinha7@users.noreply.github.com>
2024-04-19 18:13:19 +0000

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,12 @@
+let myDate= new Date()
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+
+// let myDate1= new Date(2024,3,19)
+// console.log(myDate1.toDateString())
+
+myDate.toLocaleString('default',{
+    weekday:"long"
+    
+})
