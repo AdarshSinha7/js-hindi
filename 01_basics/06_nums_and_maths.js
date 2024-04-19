cd4eccbfb86752078a4b91bd916e7c863cc49b41
Adarsh Sinha <75273319+AdarshSinha7@users.noreply.github.com>
2024-04-19 17:46:30 +0000

    Math and num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,13 @@
+const balance=new Number(100)
+console.log(balance)
+console.log(balance.toString())
+
+console.log(balance.toFixed(2))
+
+const hundreds=1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1)+min))
